baffb002dcffc2602bf5a13f3aeec667059055d6
Gustavo <gustavo.penedo@fatec.sp.gov.br>
2023-05-13 14:39:04 -0300

    Alterações na classe Assinante

--- src/Assinante.java
+++ src/Assinante.java
@@ -1,21 +1,26 @@
 public class Assinante{
+    //Atributos
     private long cpf;
     private String nome;
     private int numero;
     protected int numChamadas; // Ele está definido como protegido, o que permite que seja acessado pelas subclasses de "Assinante".
 
-    public Assinante (long cpf, String nome, int numero){ // Construtor
+    // Construtor  (No momento da criação, Getter and Setter é para alteração)
+    public Assinante (long cpf, String nome, int numero){
         this.cpf = cpf;
         this.nome = nome;
         this.numero = numero;
     }
 
+    //Métodos
+    //Método getCpf, para trazer o CPF
     public long getCpf() { // Retorna o valor do atributo "cpf"
         return cpf;
     }
 
+    //Método toString Retorna uma String com os valores dos atributos "nome", "cpf" e "numero".
     @Override
-    public String toString(){ // Retorna uma String com os valores dos atributos "nome", "cpf" e "numero".
+    public String toString(){
         return "Assinante{" +
                 "Nome: " + nome + '\n' +
                 "CPF: " + cpf + '\n' +
