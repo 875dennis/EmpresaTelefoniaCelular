1e8a2b07fa681cbdc5f60bb7ff83e1bc89cea18d
Vinicius <viniprieto@outlook.com>
2023-05-20 16:16:00 -0300

    Commit 2 corrigido Vinicius

--- src/Telefonia.java
+++ src/Telefonia.java
@@ -56,5 +56,112 @@ public class Telefonia {
         }
     }
 
+    public void listarAssinantes() {
+        System.out.println("\nAssinantes pré-pagos:");
+        for(int i=0; i<numPrePagos; i++) {
+            System.out.println(prePagos[i].toString());
+        }
+        System.out.println("Assinantes pós-pagos:");
+        for(int i=0; i<numPosPagos; i++) {
+            System.out.println(posPagos[i].toString());        }
+    }
+
+    public void fazerChamada() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite o CPF do assinante pré-pago:");
+        long cpf = sc.nextLong();
+
+        PrePago assinante_PrePago = localizarPrePago(cpf);
+        PosPago assinante_PosPago = localizarPosPago(cpf);
+
+        if (assinante_PrePago != null) {
+            System.out.println("Informe a duração da chamada:");
+            int duracao = sc.nextInt();
+            System.out.println("Informe a data da chamada (dd/mm/aaaa):");
+            GregorianCalendar data = new GregorianCalendar();
+            System.out.print("Digite o ano (2023): ");
+            int ano = sc.nextInt();
+            System.out.print("Digite o mês (Janeiro = 1): ");
+            int mes = sc.nextInt() - 1; // O mês começa em 0, por isso subtraímos 1
+            System.out.print("Digite o dia: ");
+            int dia = sc.nextInt();
+
+            data.set(GregorianCalendar.YEAR, ano);
+            data.set(GregorianCalendar.MONTH, mes);
+            data.set(GregorianCalendar.DAY_OF_MONTH, dia);
+
+            assinante_PrePago.fazerChamada(data, duracao);
+
+        }
+        else if (assinante_PosPago != null) {
+            System.out.println("Informe a duração da chamada:");
+            int duracao = sc.nextInt();
+            System.out.println("Informe a data da chamada (dd/mm/aaaa):");
+            GregorianCalendar data = new GregorianCalendar();
+            System.out.print("Digite o ano (2023): ");
+            int ano = sc.nextInt();
+            System.out.print("Digite o mês (Janeiro = 1): ");
+            int mes = sc.nextInt() - 1; // O mês começa em 0, por isso subtraímos 1
+            System.out.print("Digite o dia: ");
+            int dia = sc.nextInt();
+
+            data.set(GregorianCalendar.YEAR, ano);
+            data.set(GregorianCalendar.MONTH, mes);
+            data.set(GregorianCalendar.DAY_OF_MONTH, dia);
+
+            assinante_PosPago.fazerChamada(data, duracao);
+        }
+        else {
+            System.out.println("Assinante não encontrado.");
+
+        }
+    }
+
+    public void fazerRecarga() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite o CPF do assinante pré-pago:");
+        long cpf = sc.nextLong();
+
+        PrePago assinante = localizarPrePago(cpf);
+        if (assinante == null) {
+            System.out.println("Assinante não encontrado.");
+        } else {
+
+            System.out.println("Informe o valor da recarga:");
+            float valor = sc.nextFloat();
+            System.out.println("Informe a data da recarga (dd/mm/aaaa):");
+            GregorianCalendar data = new GregorianCalendar();
+            System.out.print("Digite o ano (2023): ");
+            int ano = sc.nextInt();
+            System.out.print("Digite o mês (Janeiro = 1): ");
+            int mes = sc.nextInt() - 1; // O mês começa em 0, por isso subtraímos 1
+            System.out.print("Digite o dia: ");
+            int dia = sc.nextInt();
+
+            data.set(GregorianCalendar.YEAR, ano);
+            data.set(GregorianCalendar.MONTH, mes);
+            data.set(GregorianCalendar.DAY_OF_MONTH, dia);
+
+            assinante.fazerRecarga(data, valor);
+        }
+    }
+
+    public PrePago localizarPrePago(long cpf) {
+        for (int i = 0; i < numPrePagos; i++) {
+            if (prePagos[i].getCpf() == cpf) {
+                return prePagos[i];
+            }
+        }
+        return null;
+    }
+
+    public PosPago localizarPosPago(long cpf) {
+        for (int i = 0; i < numPosPagos; i++) {
+            if (posPagos[i].getCpf() == cpf) {
+                return posPagos[i];
+            }
+        }
+        return null;
+    }
 
 }
